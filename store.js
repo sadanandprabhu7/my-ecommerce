e5a0211c99396499be8751c828b5e84c4a4fb4ce
sadanandprabhu7 <sadanandprabhu7@gmail.com>
2022-11-28 22:23:15 +0530

    pagination

--- store.js
+++ store.js
@@ -12,21 +12,72 @@ close.addEventListener("click", () => {
 });
 
 // LOADING ALL THE PRODUCT ON SITE FROM DATABASE WHENEVR SITE IS VISITED
+
 const parentDiv = document.getElementsByClassName("shop-items")[0];
-window.addEventListener("load", () => {
-  axios.get("http://localhost:3000/products").then((products) => {
-    products.data.productDetails.forEach((product) => {
-      const itemsDiv = `<div class="shop-item">
+const pagination = document.getElementById("pagination");
+window.addEventListener("DOMContentLoaded", () => {
+  const page = 1;
+  axios.get(`http://localhost:3000/products?page=${page}`).then((res) => {
+    console.log(res.data.products);
+    console.log(res.data);
+    productList(res.data);
+    showProducts(res.data.products);
+  });
+});
+
+function showProducts(products) {
+  parentDiv.innerHTML = "";
+  products.forEach((product) => {
+    const itemsDiv = `<div class="shop-item">
                     <span class="shop-item-title">${product.title}</span>
                     <img class="shop-item-image" src="${product.image}">
                     <div class="shop-item-details">
                         <span class="shop-item-price">$${product.price}</span>
                         <button class="btn btn-primary shop-item-button" onclick="addToCart(${product.id})" type="button">ADD TO CART</button>  
                     </div>`;
-      parentDiv.innerHTML += itemsDiv;
-    });
+    parentDiv.innerHTML += itemsDiv;
   });
-});
+}
+
+function productList({
+  currentPage,
+  hasNextPage,
+  nextPage,
+  hasPreviousPage,
+  priviousPage,
+  lastPage,
+}) {
+  pagination.innerHTML = "";
+  if (hasPreviousPage) {
+    const btn2 = document.createElement("button");
+    btn2.innerHTML = priviousPage;
+    btn2.addEventListener("click", getProducts(priviousPage));
+    pagination.appendChild(btn2);
+  }
+  const btn1 = document.createElement("button");
+  btn1.innerHTML = `<h3>${currentPage}</h3>`;
+  btn1.addEventListener("click", () => getProducts(nextPage));
+  pagination.appendChild(btn1);
+
+  if (hasNextPage) {
+    const btn3 = document.createElement("button");
+    btn3.innerHTML = nextPage;
+    btn3.addEventListener("click", () => getProducts(nextPage));
+    pagination.appendChild(btn3);
+  }
+  if (lastPage) return (currentPage = 1);
+}
+function getProducts(page) {
+  axios
+    .get(`http://localhost:3000/products?page=${page}`)
+    .then((res) => {
+      productList(res.data);
+      showProducts(res.data.products);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 ///  ADD TO CART FUNCTION
 function addToCart(product) {
